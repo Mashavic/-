e7359b03c4237756301c64f7e2ab2d3467fb0264
Alexey Sukhanov <Sukhanov.Alexey.A@yandex.ru>
2024-02-25 04:21:03 -0500

    add map, character, movement

--- src/game.c
+++ src/game.c
@@ -1,8 +1,8 @@
 #include "global.h"
-
+char map[ROWS][COLS];
 int game(void) {
   /*          player          */
-  char player_name[12];
+  // char player_name[12];
   // int player_wallet = 0;
   // int player_hp = 50;
   // int player_damage = 10;
@@ -13,10 +13,29 @@ int game(void) {
   // uint8_t goblin_gold = goblin_gold_generate();
   // printf("%d\n", goblin_gold);
 
-  player_get_name(player_name);
+  // player_get_name(player_name);
+
+  int command_player;
+  int y = 11, x = 11;
+
+  initscr();
+  keypad(stdscr, TRUE);  // on addition keys
+  noecho();              // don`t display input
+  curs_set(0);           // off curs
+
+  do {
+    clear();
+    dungeon();
+    movement(command_player, &y, &x);
+
+  } while ((command_player = getch()) != 27);  // 27 - ESC (STOP while)
+
+  getch();
+  endwin();
 
   return 0;
 }
+
 int player_get_name(char *player_name) {
   int len = 0;
   int temp;
@@ -44,4 +63,31 @@ uint8_t goblin_gold_generate() {
   goblin_gold = rand() % 5;
 
   return goblin_gold;
+}
+
+void dungeon(void) {
+  for (int yy = 0; yy <= ROWS; yy++) {
+    for (int xx = 0; xx <= COLS; xx++) {
+      if (yy == 0 || xx == 0 || yy == ROWS || xx == COLS) {
+        map[yy][xx] = '#';
+        mvaddch(yy, xx, '#');
+      }
+    }
+  }
+}
+
+void movement(int command_player, int *y, int *x) {
+  int temp_y = *y;
+  int temp_x = *x;
+  if ((command_player == UP) && (map[temp_y - 1][temp_x] != '#'))
+    temp_y--;
+  else if ((command_player == DOWN) && (map[temp_y + 1][temp_x] != '#'))
+    temp_y++;
+  else if ((command_player == LEFT) && (map[temp_y][temp_x - 1] != '#'))
+    temp_x--;
+  else if ((command_player == RIGHT) && (map[temp_y][temp_x + 1] != '#'))
+    temp_x++;
+  *y = temp_y;
+  *x = temp_x;
+  mvaddch(temp_y, temp_x, CHARACTER);  // print cursor
 }
--- src/global.h
+++ src/global.h
@@ -8,11 +8,23 @@
 #include <string.h>
 #include <time.h>
 
+#define UP 119
+#define DOWN 115
+#define LEFT 97
+#define RIGHT 100
+
+#define ROWS 20  // Y
+#define COLS 80  // X
+
+#define CHARACTER '@'
+
 /*          BASIC           */
 int game(void);
 
 /*          OTHER_FUNC      */
 int player_get_name(char *player_name);
 uint8_t goblin_gold_generate();
+void dungeon(void);
+void movement(int command_player, int *y, int *x);
 
 #endif
